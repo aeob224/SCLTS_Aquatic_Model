bd6e8d18b03c80881a1a0c391fd6e25762ca41e3
aeob224 <aidan.obrien823@gmail.com>
2025-11-22 18:15:43 -0500

    Added an initial attempt to plot the depth GAM

--- Scripts/Analysis/Bivariate Fit of Depth with Dry Ponds.R
+++ Scripts/Analysis/Bivariate Fit of Depth with Dry Ponds.R
@@ -5,7 +5,7 @@ library(lme4)
 library(lmerTest)
 library(tidyverse)
 library(mgcv)
-data <- read_csv("data_with_dry_ponds.csv")
+data <- read_csv("Data/data_with_dry_ponds.csv")
 
 ##Depth linear mixed effects model 
 depth_model <- lmer(log_larv_dens ~ depth + (1|pond), data = data)
@@ -19,4 +19,7 @@ data$pond <- as.factor(data$pond)
 depth_GAM <- gam(log_larv_dens ~ s(depth) + s(pond, bs = 're'), data = data)
 summary(depth_GAM)
 plot(depth_GAM)
-#Significant again, with declines ocurring at very shallow or very deep extremes
+#Significant again, with declines occurring at very shallow or very deep extremes
+
+#Plotting the GAM
+plot(ggeffects::ggpredict(depth_GAM, terms = 'depth', facets = FALSE))
