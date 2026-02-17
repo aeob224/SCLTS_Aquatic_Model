6d55d2be930e46825c5868eb0118d2a72cfedd96
aeob224 <aidan.obrien823@gmail.com>
2026-02-17 10:10:55 -0500

    Updated azolla water quality code to check for relationships with nitrates, DO, and pH.

--- Scripts/Analysis/Azolla_Plankton Relationship.R
+++ Scripts/Analysis/Azolla_Plankton Relationship.R
@@ -1,11 +1,35 @@
-#Relationship between Azolla and Plankton
+#Relationship between Azolla and water quality variables
 library(tidyverse)
 library(lme4)
-data <- read_csv("Data/model_data_no_ysi.csv")
+library(rqs)
+
+data <- read_csv("Data/ysi_included.csv")
+options(scipen = 999)
 
 data$pond <- as.character(data$pond)
-PlanktonAzollaModel <- lmer(log_plankton ~ azolla_presence_absence + (1|pond), data = data)
+PlanktonAzollaModel <- lmer(log_plankton ~ azolla + (1|pond), data = data)
 summary(PlanktonAzollaModel)
 anova(PlanktonAzollaModel)
 
 #No significant difference in plankton by presence/absence of Azolla
+
+
+# Check Azolla against pH
+pHAzollaModel <- lmer(log_pH ~ azolla + (1|pond), data = data)
+summary(pHAzollaModel)
+anova(pHAzollaModel)
+rsq::rsq.lmm(pHAzollaModel)
+
+
+# Azolla against nitrates
+nitrateAzolla <- lmer(log_nitrates ~ azolla + (1|pond), data = data)
+summary(nitrateAzolla)
+anova(nitrateAzolla)
+rsq::rsq.lmm(nitrateAzolla)
+
+
+# Azolla against DO
+DOazolla <- lmer(log_DO ~ azolla + (1|pond), data = data)
+summary(DOazolla)
+anova(DOazolla)
+rsq::rsq.lmm(DOazolla)
